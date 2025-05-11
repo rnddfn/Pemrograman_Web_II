2688d6ff8f4bd6a4fd63a817a0fc00a92398290f
rnddfn <130640124+rnddfn@users.noreply.github.com>
2025-05-11 23:10:36 +0800

    Delete Modul5 directory

--- Modul5/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $hostname = "localhost";
-    $database = "db_perpustakaan";
-    $user = "root";
-    $password = "";
-
-    $koneksi = mysqli_connect($hostname, $user, $password, $database);
-?>
