572c2297750b4efdc34db434275693a9a1645ff5
Randy Febrian <rnddfebrian@gmail.com>
2025-05-14 22:05:10 +0800

    Commit

--- app/Config/Routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'berandaController::index');
-$routes->get('/profil', 'profilController::profil');
--- app/Models/berandaModel.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Models;
-
-class berandaModel {
-    protected $data = ['nama' => 'Randy Febrian', 'nim' => '2310817110013'];
-
-    public function getPraktikan() {return $this -> data;}
-}
-?>
