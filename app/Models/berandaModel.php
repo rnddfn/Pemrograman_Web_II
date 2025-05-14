b8d7e18af3b7e587f6a9eec6f1a5266710daaf78
Randy Febrian <rnddfebrian@gmail.com>
2025-05-14 21:41:16 +0800

    First Commit

---
+++ app/Models/berandaModel.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Models;
+
+class berandaModel {
+    protected $data = ['nama' => 'Randy Febrian', 'nim' => '2310817110013'];
+
+    public function getPraktikan() {return $this -> data;}
+}
+?>
